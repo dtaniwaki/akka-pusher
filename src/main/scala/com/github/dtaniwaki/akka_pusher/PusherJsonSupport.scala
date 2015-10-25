03319b097314bc4471e593c8df69b57ddd75be63
dtaniwaki <daisuketaniwaki@gmail.com>
2015-10-25 13:49:24 +0900

    Remove unnecessary json format

--- src/main/scala/com/github/dtaniwaki/akka_pusher/PusherJsonSupport.scala
+++ src/main/scala/com/github/dtaniwaki/akka_pusher/PusherJsonSupport.scala
@@ -9,17 +9,6 @@ trait PusherJsonSupport extends DefaultJsonProtocol {
   import PusherModels._
   import PusherEvents._
 
-  private implicit object DateTimeJsonFormat extends RootJsonFormat[DateTime] {
-    private lazy val format = ISODateTimeFormat.dateTimeNoMillis()
-
-    def write(datetime: DateTime): JsValue = JsString(format.print(datetime.withZone(DateTimeZone.UTC)))
-
-    def read(json: JsValue): DateTime = json match {
-      case JsString(x) => format.parseDateTime(x)
-      case x           => deserializationError("Expected DateTime as JsString, but got " + x)
-    }
-  }
-
   implicit val ChannelOccupiedEventJsonSupport = jsonFormat(ChannelOccupiedEvent.apply, "name", "channel")
   implicit val ChannelVacatedEventJsonSupport = jsonFormat(ChannelVacatedEvent.apply, "name", "channel")
   implicit val MemberAddedEventJsonSupport = jsonFormat(MemberAddedEvent.apply, "name", "channel", "user_id")
